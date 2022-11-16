5f10769582921da79032cb7a70bbc9213fce7771
TrNgTien <ngoctien27122001@gmail.com>
2022-11-16 20:48:01 +0700

    Tien | Add | Initialize Game 2 - Math

---
+++ lib/screens/math_domain/game_2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Game2 extends StatefulWidget {
+  const Game2({super.key});
+
+  @override
+  State<Game2> createState() => _Game2State();
+}
+
+class _Game2State extends State<Game2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
