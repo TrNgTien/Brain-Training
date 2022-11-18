79dadf369b4bef7697a4b81e5841848c5bef2046
Phuc Duong <66458634+Bill-Fashion@users.noreply.github.com>
2022-11-18 12:01:42 +0700

    Finish Feat/game 1 math (#1)
    * Tien | Add | Configuration folder
    
    * Tien | Adjust | Re-config main app
    
    * Tien | Adjust | Adjust naming convention
    
    * Tien | Adjust | Adjust structure
    
    * Phuc | Add | Complete math_game_1
    
    * Phuc | Add | Complete math_game_1
    
    * Phuc | Adjust | Change math_game json file dir
    
    * Update lib/screens/math_domain/math_game_one.dart
    
    Co-authored-by: Tien Tran <66483590+TrNgTien@users.noreply.github.com>
    
    * Update lib/screens/math_domain/math_game_one.dart
    
    Co-authored-by: Tien Tran <66483590+TrNgTien@users.noreply.github.com>
    
    Co-authored-by: TrNgTien <ngoctien27122001@gmail.com>
    Co-authored-by: Tien Tran <66483590+TrNgTien@users.noreply.github.com>

---
+++ lib/constants/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const primaryBlue = Color.fromARGB(255, 58, 68, 229);
+const primaryBlue50 = Color.fromARGB(50, 58, 68, 229);
+const primaryBlue75 = Color.fromARGB(75, 58, 68, 229);
+const primaryBlue25 = Color.fromARGB(25, 58, 68, 229);
+const black50 = Color.fromARGB(50, 0, 0, 0);
+const textNameColor = Color.fromARGB(255, 14, 17, 57);
+const grayColor = Color.fromARGB(255, 134, 136, 156);
+const grayColor50 = Color.fromARGB(255, 232, 232, 232);
+const black75 = Color.fromARGB(75, 0, 0, 0);
+const black85 = Color.fromARGB(255, 219, 219, 225);
+const orangeColor = Color.fromARGB(255, 249, 99, 52);
+const orangeColor75 = Color.fromARGB(75, 249, 99, 52);
+
+const borderRadius30 = Radius.circular(30);
