9a4abe5900800306bc3fec0312d749fee17af9cd
Viet Anh Vo <anhviet5121pc@gmail.com>
2022-11-20 18:01:27 +0700

    Viet | Add | Initial structure

---
+++ lib/screens/language_domain/language_game_four.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageGameFour extends StatefulWidget {
+  const LanguageGameFour({super.key});
+
+  @override
+  State<LanguageGameFour> createState() => _LanguageGameFourState();
+}
+
+class _LanguageGameFourState extends State<LanguageGameFour> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
