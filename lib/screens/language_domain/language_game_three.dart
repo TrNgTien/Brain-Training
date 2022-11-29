a13c899d9ba87b4da0c8df840c056792ce5573dc
Viet Anh Vo <anhviet5121pc@gmail.com>
2022-11-29 13:05:52 +0700

    Viet | Add | Initial layout

--- lib/screens/language_domain/language_game_three.dart
+++ lib/screens/language_domain/language_game_three.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class LanguageGameThree extends StatefulWidget {
   const LanguageGameThree({super.key});
@@ -8,15 +9,109 @@ class LanguageGameThree extends StatefulWidget {
 }
 
 class _LanguageGameThreeState extends State<LanguageGameThree> {
+  final int answerDurationInSeconds = 60;
+  Duration answerDuration = const Duration();
+  Timer? countdownTimer;
+  int _point = 0;
+
+  // Timer Handler
+  void startTimer() {
+    answerDuration = Duration(seconds: answerDurationInSeconds);
+
+    countdownTimer =
+        Timer.periodic(const Duration(seconds: 1), (_) => setCountDown());
+  }
+
+  void setCountDown() {
+    const reduceSecondsBy = 1;
+    setState(() {
+      final seconds = answerDuration.inSeconds - reduceSecondsBy;
+      if (seconds < 0) {
+        setCancelTimer();
+      } else {
+        answerDuration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  void setCancelTimer() {
+    countdownTimer!.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    setCancelTimer();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final seconds = answerDuration.inSeconds;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Language Game Three'),
       ),
-      body: const Center(
-        child: Text('Language Game Three'),
-      ),
+      body: SingleChildScrollView(
+          child: Column(children: [
+        Text(
+          '$seconds',
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 50),
+        ),
+        Text(
+          "Điểm: $_point",
+          style: const TextStyle(fontSize: 30, color: Colors.red),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const Text("Hãy điền tiếng tiếp theo để tạo nên một từ có nghĩa",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 25)),
+        const SizedBox(
+          height: 60,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Container(
+              color: Colors.blue[600],
+              width: 180,
+              height: 120,
+              alignment: Alignment.center,
+              child: const Text("Hello",
+                  style: TextStyle(fontSize: 30, color: Colors.white))),
+          Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2)),
+              width: 180,
+              height: 120,
+              alignment: Alignment.center,
+              child: const TextField(
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                  border: InputBorder.none,
+                  hintText: "Nhập từ",
+                ),
+              ))
+        ]),
+        const SizedBox(
+          height: 120,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 14),
+              textStyle: const TextStyle(fontSize: 24)),
+          child: const Text("Kiểm tra"),
+        )
+      ])),
     );
   }
 }
