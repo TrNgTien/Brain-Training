b9910678c4587abf8a080b117f45abbb5d93e101
Viet Anh Vo <anhviet5121pc@gmail.com>
2022-11-29 16:34:21 +0700

    Viet | Add | Get the first character

--- lib/screens/language_domain/language_game_three.dart
+++ lib/screens/language_domain/language_game_three.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -13,6 +16,8 @@ class _LanguageGameThreeState extends State<LanguageGameThree> {
   Duration answerDuration = const Duration();
   Timer? countdownTimer;
   int _point = 0;
+
+  late Future<String> firstCharacter;
 
   // Timer Handler
   void startTimer() {
@@ -38,9 +43,23 @@ class _LanguageGameThreeState extends State<LanguageGameThree> {
     countdownTimer!.cancel();
   }
 
+  // Fetch Data
+  Future<String> fetchRandomCharacter() async {
+    final response =
+        await http.get(Uri.parse("http://localhost:8080/api/language/word"));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data["word"].split(" ")[0];
+    }
+
+    throw Exception('Failed to load album');
+  }
+
   @override
   void initState() {
     super.initState();
+
+    firstCharacter = fetchRandomCharacter();
     startTimer();
   }
 
@@ -59,59 +78,75 @@ class _LanguageGameThreeState extends State<LanguageGameThree> {
         title: const Text('Language Game Three'),
       ),
       body: SingleChildScrollView(
-          child: Column(children: [
-        Text(
-          '$seconds',
-          style: const TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 50),
-        ),
-        Text(
-          "Điểm: $_point",
-          style: const TextStyle(fontSize: 30, color: Colors.red),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        const Text("Hãy điền tiếng tiếp theo để tạo nên một từ có nghĩa",
-            textAlign: TextAlign.center, style: TextStyle(fontSize: 25)),
-        const SizedBox(
-          height: 60,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Container(
-              color: Colors.blue[600],
-              width: 180,
-              height: 120,
-              alignment: Alignment.center,
-              child: const Text("Hello",
-                  style: TextStyle(fontSize: 30, color: Colors.white))),
-          Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2)),
-              width: 180,
-              height: 120,
-              alignment: Alignment.center,
-              child: const TextField(
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
-                  border: InputBorder.none,
-                  hintText: "Nhập từ",
-                ),
-              ))
-        ]),
-        const SizedBox(
-          height: 120,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 14),
-              textStyle: const TextStyle(fontSize: 24)),
-          child: const Text("Kiểm tra"),
-        )
-      ])),
+          child: FutureBuilder<String>(
+        future: firstCharacter,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(children: [
+              Text(
+                '$seconds',
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 50),
+              ),
+              Text(
+                "Điểm: $_point",
+                style: const TextStyle(fontSize: 30, color: Colors.red),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text("Hãy điền tiếng tiếp theo để tạo nên một từ có nghĩa",
+                  textAlign: TextAlign.center, style: TextStyle(fontSize: 25)),
+              const SizedBox(
+                height: 60,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Container(
+                    color: Colors.blue[600],
+                    width: 180,
+                    height: 120,
+                    alignment: Alignment.center,
+                    child: Text(snapshot.data!,
+                        style: const TextStyle(
+                            fontSize: 30, color: Colors.white))),
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)),
+                    width: 180,
+                    height: 120,
+                    alignment: Alignment.center,
+                    child: const TextField(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                        border: InputBorder.none,
+                        hintText: "Nhập từ",
+                      ),
+                    ))
+              ]),
+              const SizedBox(
+                height: 120,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 14),
+                    textStyle: const TextStyle(fontSize: 24)),
+                child: const Text("Kiểm tra"),
+              )
+            ]);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      )),
     );
   }
 }
