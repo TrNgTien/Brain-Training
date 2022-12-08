6412ac0327aa521796f3da5998d8f9c01e2a1d64
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-12-08 21:51:09 +0700

    Phuc | Rebase | Rebase from testing to feat/game-3-memory

--- lib/screens/memory_domain/memory_game_3.dart
+++ lib/screens/memory_domain/memory_game_3.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
+import 'package:flutter/services.dart';
 
 class MemoryGame3 extends StatefulWidget {
   const MemoryGame3({Key? key}) : super(key: key);
@@ -11,32 +13,20 @@ class MemoryGame3 extends StatefulWidget {
 }
 
 class _MemoryGame3State extends State<MemoryGame3> {
+  final random = Random();
   List<String> getList(int n, List<String> listSource) => listSource.sample(n);
+
   Timer? countdownTimer;
 
-  List<String> list = [
-    'aaa',
-    'bbb',
-    'ccc',
-    'ddd',
-    'eee',
-    'fff',
-    'ggg',
-    'hhh',
-    'iii',
-    'kkk',
-    'lll',
-    'mmm',
-    'nnn',
-    'ooo',
-    'ppp',
-    'qqq',
-    'sss'
-  ];
+  List<String> list = [];
   List<String> initCards = [];
   List<String> answers = [];
   List<String> questions = [];
   List<String> questionsSelected = [];
+  List<String> animalImages = [];
+  List<String> transportationImages = [];
+  List<List<String>> l1 = [[]];
+
   List<bool> isSelected = <bool>[];
   int point = 0;
   int round = 1;
@@ -46,7 +36,6 @@ class _MemoryGame3State extends State<MemoryGame3> {
   bool isEnd = false;
   bool isAnswer = false;
   late Duration myDuration = Duration(seconds: seconds);
-  final random = Random();
 
   void handleSubmit() => {
         setState(() => {
@@ -72,7 +61,7 @@ class _MemoryGame3State extends State<MemoryGame3> {
       addTimer(3);
       changeLevel();
       startTimer();
-      initCards = [...getList(4, list)];
+      initCards = getList(4, list);
     });
   }
 
@@ -105,10 +94,13 @@ class _MemoryGame3State extends State<MemoryGame3> {
 
   void setQuestions() => {
         setState(() {
-          answers = [...initCards.sublist(initCards.length - level)];
-          initCards.fillRange(initCards.length - level, initCards.length, '?');
-          initCards.shuffle();
-          createQuestions(level + 2);
+          if (initCards.isNotEmpty) {
+            answers = [...initCards.sublist(initCards.length - level)];
+            initCards.fillRange(
+                initCards.length - level, initCards.length, '?');
+            initCards.shuffle();
+            createQuestions(level + 2);
+          }
         }),
       };
   void createQuestions(int nAnsCards) => {
@@ -188,11 +180,35 @@ class _MemoryGame3State extends State<MemoryGame3> {
     countdownTimer!.cancel();
   }
 
+  Future _initImages() async {
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    final imagePaths1 = manifestMap.keys
+        .where((String key) => key.contains('images/'))
+        .where((String key) => key.contains('Animal/'))
+        .toList();
+    final imagePaths2 = manifestMap.keys
+        .where((String key) => key.contains('images/'))
+        .where((String key) => key.contains('Transportation/'))
+        .toList();
+
+    setState(() {
+      list = imagePaths1;
+      initCards = getList(4, imagePaths1);
+
+      // l1[0] = imagePaths1;
+      // l1[1] = imagePaths2;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    _initImages();
     seconds = 3;
-    initCards = [...getList(4, list)];
+    // getList(4, list);
     startTimer();
   }
 
@@ -205,10 +221,14 @@ class _MemoryGame3State extends State<MemoryGame3> {
   @override
   Widget build(BuildContext context) {
     final seconds = myDuration.inSeconds;
+    // print(l1[1]);
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text("Màn " + round.toString()),
+        ),
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
                 '$seconds',
@@ -221,35 +241,75 @@ class _MemoryGame3State extends State<MemoryGame3> {
                 "Điểm: $point",
                 style: const TextStyle(fontSize: 30, color: Colors.red),
               ),
-              GridView.count(
-                crossAxisCount: 4,
-                shrinkWrap: true,
-                children: initCards
-                    .map((e) => Center(
-                          child: Text(
-                            e,
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        ))
-                    .toList(),
-              ),
-              const Text("Chon 3 dap an"),
-              questions.isNotEmpty ? ansToggleBtn() : Container(),
-              isEnd
+              list.isNotEmpty
                   ? GridView.count(
                       crossAxisCount: 4,
                       shrinkWrap: true,
-                      children: answers
-                          .map((e) => Center(
-                                child: Text(
-                                  e,
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                              ))
+                      children: initCards
+                          .map(
+                            (e) => e != '?'
+                                ? Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.blue),
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(8)),
+                                    child: Center(
+                                      child: Image.asset(
+                                        e,
+                                        height: 50,
+                                        width: 50,
+                                      ),
+                                    ))
+                                : Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.blue),
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(8)),
+                                    child: Center(
+                                        child: Text("?",
+                                            style: TextStyle(fontSize: 50))),
+                                  ),
+                          )
                           .toList(),
                     )
                   : Container(),
-              Row(
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  "Chọn $level hình",
+                ),
+              ),
+              questions.isNotEmpty ? ansToggleBtn() : Container(),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Đáp án",
+                ),
+              ),
+              isEnd
+                  ? Container(
+                      child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: answers
+                          .map((e) => e != '?'
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue),
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Center(
+                                    child: Image.asset(
+                                      e,
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                  ),
+                                )
+                              : Text("?", style: TextStyle(fontSize: 10)))
+                          .toList(),
+                    ))
+                  : Container(),
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
@@ -258,9 +318,14 @@ class _MemoryGame3State extends State<MemoryGame3> {
                                   !isEnd
                               ? handleSubmit
                               : null,
-                      child: const Text("check")),
-                  ElevatedButton(
-                      onPressed: resetRound, child: const Text("reset"))
+                      child: const Text("Kiểm tra")),
+                  round < 10
+                      ? ElevatedButton(
+                          onPressed: resetRound,
+                          child: const Text(
+                            "Vòng tiếp theo",
+                          ))
+                      : Container()
                 ],
               )
             ],
@@ -268,11 +333,10 @@ class _MemoryGame3State extends State<MemoryGame3> {
         ));
   }
 
-  Ink ansToggleBtn() {
+  Widget ansToggleBtn() {
     return Ink(
-      width: 200,
-      height: 100,
-      color: Colors.white,
+      width: MediaQuery.of(context).size.width * 0.75,
+      height: MediaQuery.of(context).size.height * 0.25,
       child: GridView.count(
         primary: true,
         crossAxisCount: level == 2 ? 3 : 4,
@@ -280,7 +344,7 @@ class _MemoryGame3State extends State<MemoryGame3> {
         mainAxisSpacing: 8,
         children: List.generate(isSelected.length, (index) {
           return InkWell(
-            splashColor: Colors.yellow,
+            highlightColor: Color.fromRGBO(83, 179, 248, 1),
             onTap: () {
               setState(() {
                 for (int indexBtn = 0;
@@ -298,15 +362,19 @@ class _MemoryGame3State extends State<MemoryGame3> {
               });
             },
             child: Ink(
-                decoration: BoxDecoration(
-                  // image: DecorationImage(image: ),
-                  color: isSelected[index]
-                      ? const Color.fromARGB(255, 83, 179, 248)
-                      : Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(color: Colors.blue),
-                ),
-                child: Text(questions[index])),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(questions[index]),
+                    colorFilter: isSelected[index]
+                        ? ColorFilter.mode(
+                            Colors.black.withOpacity(0.2), BlendMode.dstATop)
+                        : null),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: Colors.blue),
+              ),
+              // child: Text(questions[index]),
+            ),
           );
         }),
       ),
