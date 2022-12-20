6b9f62c8d947c50b8bc3ed519ce0ece8df04522d
Viet Anh Vo <anhviet5121pc@gmail.com>
2022-12-20 12:19:40 +0700

    Viet | Add | Setup for attention game 1

---
+++ lib/screens/games_screen/attention_domain/attention_game_one.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AttentionGameOne extends StatefulWidget {
+  const AttentionGameOne({super.key});
+
+  @override
+  State<AttentionGameOne> createState() => _AttentionGameOneState();
+}
+
+class _AttentionGameOneState extends State<AttentionGameOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
