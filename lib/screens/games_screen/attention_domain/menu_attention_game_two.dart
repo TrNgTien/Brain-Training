a6d0c6b8c52c699169ca25fcb135cb1a65de0cc8
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-12-25 17:42:52 +0700

    Phuc | Adjust | redesign game-2-attention

--- lib/screens/games_screen/attention_domain/menu_attention_game_two.dart
+++ lib/screens/games_screen/attention_domain/menu_attention_game_two.dart
@@ -1,3 +1,4 @@
+import 'package:brain_training/constants/color.dart';
 import 'package:brain_training/screens/games_screen/attention_domain/attention_game_two.dart';
 import 'package:brain_training/screens/games_screen/attention_domain/enum.dart';
 import 'package:flutter/material.dart';
@@ -18,35 +19,59 @@ class _AttentionGame2MenuState extends State<AttentionGame2Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: yellowPastel,
+          centerTitle: true,
+          title: Text(
+            "Chọn độ khó",
+            style: TextStyle(color: darkTextColor),
+          ),
+        ),
         body: SingleChildScrollView(
           child: Center(
-            child: Column(
-                children: difficulty.entries.map((entry) {
-              switch (entry.key) {
-                case GameDifficulty.EASY:
-                  return difficultyButton(context, entry.key, entry.value);
-                case GameDifficulty.MEDIUM:
-                  return difficultyButton(context, entry.key, entry.value);
-                case GameDifficulty.HARD:
-                  return difficultyButton(context, entry.key, entry.value);
-              }
-            }).toList()),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: difficulty.entries.map((entry) {
+                    switch (entry.key) {
+                      case GameDifficulty.EASY:
+                        return difficultyButton(
+                            context, entry.key, entry.value, greenBtn);
+                      case GameDifficulty.MEDIUM:
+                        return difficultyButton(
+                            context, entry.key, entry.value, yellowBtn);
+                      case GameDifficulty.HARD:
+                        return difficultyButton(
+                            context, entry.key, entry.value, redGgBtn);
+                    }
+                  }).toList()),
+            ),
           ),
         ));
   }
 
   Widget difficultyButton(
-      BuildContext context, GameDifficulty dif, String tag) {
-    return ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AttentionGame2(difficulty: dif),
-            ),
-          );
-        },
-        child: Text(tag.toString()));
+      BuildContext context, GameDifficulty dif, String tag, Color color) {
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: color, fixedSize: Size.fromHeight(70)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AttentionGame2(difficulty: dif),
+              ),
+            );
+          },
+          child: Text(
+            tag.toString(),
+            style: TextStyle(fontSize: 25),
+          ),
+        ));
   }
 }
