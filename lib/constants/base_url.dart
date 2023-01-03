74d5067008cc68ca3ff0896264d960ffb61e4d0f
TrNgTien <ngoctien27122001@gmail.com>
2023-01-03 14:07:10 +0700

    Tien | Adjust | Adjust route request

--- lib/constants/base_url.dart
+++ lib/constants/base_url.dart
@@ -1,4 +1,4 @@
-const base_url = "https://mobile.iuweb.online/api";
+const base_url = "https://be-mobile.vercel.app/api";
 const baseUrlLanguage = "$base_url/language";
 
 // outside API for language game to fetch random characters
